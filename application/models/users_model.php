bbc665bd1a60cbc83371b8aa9a156333a59e426a
Oualid Mahri <secondspecialkey@hotmail.com>
2014-02-23 23:48:00 +0100

    yyy

---
+++ application/models/users_model.php
@@ -0,0 +1,7 @@
+<?php
+class User_model extends Model {
+     function User_model(){
+          parent::Model();
+     }
+}
+?>
---
+++ application/views/staticPages/login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=News+Cycle:400,700" rel="stylesheet" />
+<link href="<?php echo css_url('default')?>" rel="stylesheet" type="text/css" media="all" />
+<link href="<?php echo css_url('fonts')?>" rel="stylesheet" type="text/css" media="all" />
+
+<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+</head>
+<body>
+<div id="header" class="container">
+	<div id="logo">
+		<h1><a href="#">Login</a></h1>
+	</div>
+	<div id="menu">
+		<ul>
+			<li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
+			<li><a href="#" accesskey="2" title="">Our Clients</a></li>
+			<li><a href="#" accesskey="3" title="">About Us</a></li>
+			<li><a href="#" accesskey="4" title="">Careers</a></li>
+			<li><a href="#" accesskey="5" title="">Contact Us</a></li>
+		</ul>
+	</div>
+</div>
+<div id="banner">
+	<div class="container">
+		
+		
+		
+	</div>
+</div>
+<div id="copyright" class="container">
+	<p>Copyright (c) 2014 Sitename.com. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://www.freecsstemplates.org/" rel="nofollow">FreeCSSTemplates.org</a>.</p>
+</div>
+</body>
+</html>
